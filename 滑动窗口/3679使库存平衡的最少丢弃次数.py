95a2be0104e5f6847cc0e221907f95e6c62c2cd5
dersteppenwolfruowen-316 <rl948@cornell.edu>
2026-07-06 22:15:22 +0800

    定长滑动窗口 + 贪心

---
+++ 滑动窗口/3679使库存平衡的最少丢弃次数.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minArrivalsToDiscard(self, arrivals: List[int], w: int, m: int) -> int:
+        kind = defaultdict(int)
+        res = 0
+        for i, c in enumerate(arrivals):
+            if kind[c] == m: #当天等于m，丢弃
+                arrivals[i] = 0 #丢弃arrivals[i]
+                res += 1
+            else:
+                kind[c] += 1
+            
+            left  = i+1-w
+            if left >= 0:
+                kind[arrivals[left]] -= 1
+
+        return res
+            
+        
+
+        
